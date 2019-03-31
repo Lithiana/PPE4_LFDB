43ce15e945f7df48186c38d58a3d95d564289b41
Lithiana <caroline.duballet@gmail.com>
2019-03-31 19:12:03 +0200

    Search OK - Navigation OK - setState blank

---
+++ App.js
@@ -0,0 +1,20 @@
+
+// ----------------------CECI EST UN MAIN ----------------------
+// -------------------------------------------------------------
+// -------------------------IMPORT------------------------------
+
+import React from 'react';
+import Navigation from './Navigation/Navigation';
+import Search from './Components/Search'
+
+
+//---------------------RENDER DE L'APP -------------------------
+//--------------------------------------------------------------
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Navigation/>
+    )
+  }
+}
